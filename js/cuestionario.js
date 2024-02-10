c859f4d63c04936056504ab4ca918b24ec37153b
PrepaEnLineaOlimpia <20778354@prepaenlinea-sep.edu.mx>
2024-02-09 18:33:29 -0600

    versión final
    Versión final

---
+++ js/cuestionario.js
@@ -0,0 +1,350 @@
+preguntas=[
+["¿En cuántas formas pueden dividirse las pruebas del software?",
+"Dos.",
+"Tres.",
+"Cinco.",
+"Cuatro."],
+
+["Se basan en un minucioso examen de los detalles procedimentales",
+"Pruebas de caja blanca.",
+"Pruebas de caja negra.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["En estas pruebas se comprueban los caminos lógicos del software en base a examinar trozos específifos del programa.",
+"Pruebas de caja blanca.",
+"Pruebas de caja negra.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["En las pruebas de caja blanca...",
+"La idea es probar las líneas de falla más probables del software.",
+"Se trabaja bien.",
+"Se tratan de arreglar errores de la interfaz.",
+"No se hace nada de provecho."],
+
+["Su objetivo es elegir casos de prueba de modo que una determinada propiedad o característica del código tome el mayor número posible de valores.",
+"Cobertura de código..", 
+"Cobertura de bucles.",
+"Dobertura del sistema.",
+"Cobertura de requerimientos."],
+
+["¿Qué se realiza en las pruebas de bucles?",
+"Asegurar la sintaxis.",
+"Probar estructuras de datos.",
+"Probar variables.",
+"Inyección de SQL y perfiles operacionales."],
+
+["En las pruebas de caja negra...",
+"No se toma en cuenta el interior del sistema.",
+"Se pone mayor énfasis al interior del producto.",
+"No importa lo qye piense el usuario.",
+"Los resultados no importan."],
+
+["Las pruebas de caja negra se realizan sobre:",
+"La interfaz del software.",
+"Los requerimientos.",
+"Estructuras de datos.",
+"Todo el sistema."],
+
+["Pretenden demostrar que el software funciona adecuasamente:",
+"Pruebas de caja negra.",
+"Pruebas de caja blanca.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["En estas pruebas se busca que las entradas produzcan las salidas correctas:",
+"Pruebas de caja negra.",
+"Pruebas de caja blanca.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["Estas pruebas no tienen en cuenta la estructura lógica interna del software:",
+"Pruebas de caja negra.",
+"Pruebas de caja blanca.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["Cuándo se dice que las pruebas son suficientes?",
+"Cuando se asegura que se han acabado las combinaciones de entrada.",
+"Cuando se cansa el equio de trabajo.",
+"Cuando hay un solo error.",
+"Cuando tenemos muchos errores."],
+
+["Es una técnica centrada en el usuario para evaluar un producto",
+"Ninguna de las anteriores.",
+"Pruebas de caja blanca.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["Son pruebas estructurales",
+"Pruebas de caja blanca.",
+"Pruebas de caja negra.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["En estas pruebas se conoce el código y se sigue su estructura lógica",
+"Pruebas de caja blanca.",
+"Pruebas de caja negra.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["Están relacionadas con el bajo nivel de programación",
+"Pruebas de caja blanca.",
+"Pruebas de caja negra.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["Son pruebas funcionales.",
+"Pruebas de caja blanca.",
+"Pruebas de caja negra.",
+"Pruebas de caja gris.",
+"Pruebas de caja morada."],
+
+["¿Qué tipo de ´pruebas se combinan para validar el software?",
+"Pruebas de caja negra y caja blanca.",
+"Pruebas de caja blanca y caja gris.",
+"Pruebas de caja gris y caja nefra.",
+"Pruebas de caja morada y caja negra."],
+
+["¿Qué puede afectar la forma en que se prueba un componente?",
+"La visión del componenete.",
+"Ek estado de ánimo del usuario.",
+"La forma en la que se inicia el sistema.",
+"La experiencia del usuario."],
+
+["¿Cuántas formas tenemos de ver el objeto de estudio?",
+"Dos",
+"Tres",
+"Cuatro",
+"Una"],
+
+["¿Las pruebas de caja blanca y caja negra se basan, en primer plano por:?",
+"Nivel de profundidad de estudio.",
+"Nivel de enfoque.",
+"Enfoque para casos de prueba.",
+"Los planos lógicos del software."],
+
+["¿Las pruebas de caja blanca y caja negra se basan, en segundo plano por:?",
+"Enfoque para casos de prueba.",
+"Nivel de enfoque.",
+"Nivel de profuncidad de estudio.",
+"Los planos lógicos del software."],
+
+["¿A qué se refiere la cobertura de sentencias?",
+"Todas sentencia ejecutable se involucra al menos una vez.",
+"Toda sentenccia ejecutable de involucra cien veces.",
+"No todas las sentencias se deben involucrar.",
+"Cubrir de forma cinsisa todas ls sentencias."],
+
+["En este rubro se diseñan casos de prueba que ejerciten las vertientes verdadera y falsa para cada condición del programa.",
+"Cobertura de decidiones.",
+"Cobertura de condiciones",
+"Cobertura lógica",
+"Cobertura total del sistema."],
+
+["¿Qué se asegura con la cobertura de decisiones?",
+"La prueba completa de estructuras de control.",
+"Pa prueba de flujo de datos.",
+"La prueba de condiciones.",
+"La prueba del sistema global."],
+
+["¿Qué es Lo que no se debe hacer en la cobertura de decisiones?",
+"Decisiones de combinaciones lógicas.",
+"Decisiones únicamente lógicas.",
+"Deciciones que combinen bucles.",
+"Decisiones que infieren de forma global en el sistema."],
+
+["¿Qué refiere la cobertura de condiciones?",
+"Tomar valores verdadero/falso al menos una vez.",
+"Tomar valores verdadero/falso una vez.",
+"Tomar valores verdadero/falso siempre.",
+"No tomar valores verdadero/falso."],
+
+["¿Qué valores se utilizan para la verificación de condiciones?",
+"Verdadero y falso",
+"Cien y cero.",
+"Cero y uno.",
+"Verdadero y cero."],
+
+["¿Se necesita que cada decisión tome tosos sus posibles valores a la vez?",
+"No es necesario.",
+"Pruebas unitarias.",
+"Verificación formal.",
+"Perfiles operacionales."],
+
+["En este caso de prueba se consideran las decisiones y condiciones",
+"Cobertura de condición/decisión.",
+"Cobertura lógica/estructural.",
+"Cobertura de sistema.",
+"Cobertura de verdadero/falso."]
+
+["¿Qué aspecto importante toma en cuenta la condición/decisión?",
+"Cada condición afecta independientemente su decisión.",
+"Afectar cada decisión con cada variable.",
+"Cada condición afecta todas las decisiones.",
+"Afectar cada condición con variables globales.."]
+
+["Requiere la evaluación de todas las posibles combinaciones de valores de cada condición en cada decisión.",
+"Cobertura de múltiple decisión.",
+"Cobertura lógica/estructural.",
+"Cobertura de sistema.",
+"Cobertura de verdadero/falso."]
+
+["Si el objeto de prueba se ve desde fuera, se dice que se hacen pruebas de...",
+"Caja negra.",
+"Caja blanca.",
+"Caja gris.",
+"Combinaciones de todas las pruebas."]
+
+["Esta prueba consiste en alimencat la caja con entradas y anotar las salidas producidas. ",
+"Caja negra.",
+"Caja blanca.",
+"Caja gris.",
+"Combinaciones de todas las pruebas."]
+
+["Cuando se hacen pruebas ce caja negra ¿cuál es la meta de la prueba?",
+"Asegurar que se han ingresado toda clase de entrada y verificar la salida.",
+"Asegurar que la salida arroja datos completos.",
+"Asegurar que se completan los niveles internos del sistema.",
+"Asegurar que se completan los objetivos de la interfaz."]
+
+["Selecionan casos de prueba a partir de las especificaciones funcionales del software.",
+"Caja negra.",
+"Caja blanca.",
+"Caja gris.",
+"Combinaciones de todas las pruebas."]
+
+["Estas pruebas pueden aplicarse aún no implementado el código fuente.",
+"Caja negra.",
+"Caja blanca.",
+"Caja gris.",
+"Combinaciones de todas las pruebas."]
+
+["Si conocemos qué resultados debe arrojar el sistema ¿qué tipo de prueba se debe hacer?",
+"Caja negra",
+"Caja blanca.",
+"Caja gris.",
+"Combinaciones de todas las pruebas."]
+
+["¿Cuántos enfoques hay para realizar pruebas de caja negra'",
+"Tres.",
+"Dos.",
+"Uno.",
+"Cinco."]
+
+["Se realiza una división del dominio de entrada en particiones.",
+"Partición del sistema.",
+"Partición de datos.",
+"Partición lógica.",
+"Partición de condiciones."]
+
+["¿Qué se toma en cuenta en las pruebas sobre los estados del software?",
+"Los posibles escenarios en los que puede estar el sistema.",
+"Partición de datos.",
+"Partición lógica.",
+"Partición de condiciones."]
+
+["Con este enfoque, los casos se prueba se especifican como un conjunto de operaciones que deben hacerse consecutivamente sobre el sistema bajo prueba.",
+"Pruebas de flujo lógico de control.",
+"Partición de datos.",
+"Partición lógica.",
+"Partición de condiciones."]
+
+["¿Cuál es el enfoque más usado para obtener casos de prueba?",
+"Partición de clases de equivalencia.",
+"Partición de datos.",
+"Partición lógica.",
+"Partición de condiciones."]
+
+["¿En qué consiste la partición de clases de equivalencia?",
+"Partición de clases del sistema.",
+"Partición de datos.",
+"Partición lógica.",
+"Partición de condiciones."]
+
+["¿Qué ventaha tiene usar clases de equivalencia?",
+"Considerar errores dentro de clases equivalentes.",
+"Partición de datos.",
+"Partición lógica.",
+"Partición de condiciones."]
+
+["¿Cuáles son los tipos de cajas desarrolladas en el texto?",
+"Pruebas de caja negra y caja blanca.",
+"Pruebas de caja blanca y caja gris.",
+"Pruebas de caja gris y caja nefra.",
+"Pruebas de caja morada y caja negra."]
+
+["¿Qué elementos determinan la entrada de caja blanca?",
+"Elementos de diseño.",
+"Elementos lógicos.",
+"Elementos aritméticos.",
+"Elementos de alto nivel."]
+
+["¿Qué se hace con el resultado obtenido de la caja negra?",
+"Se compara con la salida esperada.",
+"Se compara con la salida real.",
+"Se compara de forma lógica..",
+"Se compara con salidas anteriores."]
+
+["¿Cuándo se dice que la prueba es dinámica?",
+"Se ejecuta el software para determinar errores.",
+"Se ejecuta el software para corregir errores.",
+"Cuando se ejecutan todas las posibilidades al mismo tiempo.",
+"Cuando se pospone la prueba estática."]];
+
+$(document).ready(function () {
+	$("#calificacion").hide();
+	seleccionadas=[];
+	contador=0;
+	while(contador<10)
+	{
+		numero=Math.floor(Math.random()*(46-0+1))+0;
+		if(seleccionadas.indexOf(numero)==-1)
+		{
+			seleccionadas.push(numero);
+			contador+=1;
+		}
+	}
+	
+	respuestas=[];
+	for(i=0;i<10;i++)
+	{
+		document.getElementById("h"+i).innerHTML=(i+1)+".-"+preguntas[seleccionadas[i]][0];
+		numero=Math.floor(Math.random()*(4))+1;
+		respuestas.push(numero);
+		for(j=0;j<4;j++)
+		{
+			document.getElementById("l"+i+j).innerHTML=preguntas[seleccionadas[i]][numero];
+			numero+=1;
+			if(numero==5)
+			{
+				numero=1;
+			}
+		}
+	}
+});
+
+function evaluar()
+{
+	$("#preguntas").hide("slow");
+	$("#calificacion").show("slow");
+	
+	puntuacion=0;
+	
+	for(i=0;i<10;i++)
+	{
+		correcta=respuestas[i]
+		if(correcta!=1)
+		{
+			correcta=6-correcta;
+		}
+		correcta-=1;
+		if(document.getElementById("r"+i+correcta).checked)
+		{
+			puntuacion+=1;
+		}
+	}
+	
+	document.getElementById("puntuacion").innerHTML="La puntuación obtenida es: "+puntuacion;
+}
